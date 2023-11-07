a25bde9a76ab0c095e96dddae3682bbca93d6ce2
SamSumanth6 <sumanthvenkatesh6@gmail.com>
2023-11-07 12:48:13 +0530

    add button

---
+++ src/components/SearchBox/index.js
@@ -0,0 +1,28 @@
+import { ReactComponent as SearchIcon } from "./SearchIcon.svg"
+
+export default()=>{
+    return <form style={{
+        display: 'flex',
+        minWidth: '570px'
+    }}>
+        <input
+        placeholder="Search a album of your choice"
+        style={{
+            flex: 1,
+            borderRadius: '8px 0px 0px 8px',
+            padding: '8px'
+        }}
+        />
+        <button style={{
+            width:'66px',
+            height:'48px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '0px 8px 8px 0px',
+            padding: '8px'
+        }}>
+            <SearchIcon width={20} height={20} />
+        </button>
+    </form>
+}
