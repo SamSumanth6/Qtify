00e9acbf7d85d420e30c209833141146f70c0db6
SamSumanth6 <sumanthvenkatesh6@gmail.com>
2023-11-12 11:18:52 +0530

    grid-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
-import card from './components/card';
-import Card from './components/card';
+import Section from './components/Section';
+
+const ENDPOINT = 'https://qtify-backend-labs.crio.do/'
 
 function App() {
+
+  const [topAlbums, setTopAlbums] = useState([])
+  const [newAlbums, setNewAlbums] = useState([])
+
+  useEffect(()=>{
+    axios.get(`${ENDPOINT}albums/top`)
+    .then( (response) =>{
+      setTopAlbums(response.data)
+    })
+    axios.get(`${ENDPOINT}albums/new`)
+    .then((response) =>{
+      setNewAlbums(response.data)
+    })
+  },[])
+
+
+
   return (
     <div>
         <Navbar/>
         <HeroSection/>
-        <Card />
+        <Section title='Top Albums' data={topAlbums}/>
+        <Section title='New Albums' data={newAlbums}/>
     </div>
   );
 }
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,17 +1,22 @@
 import './card.css'
 
-export default()=>{
+export default({
+    imgSrc,
+    followersCount,
+    label
+
+})=>{
     return (<div className='card-wrapper'>
         <div className='card'>
             <div className='card-img-frame'>
-                <img className='card-img'  src='democardimg.png'/>
+                <img className='card-img'  src={imgSrc}/>
             </div>
             <div className='card-content'>
                 <span className='card-content-pill'>
-                    100 Follows
+                    {followersCount} Follows
                 </span>
             </div>
         </div>
-        <p className='card-label'>New English Songs</p>
+        <p className='card-label'>{label}</p>
     </div>)
 }
