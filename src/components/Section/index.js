b38adccf11b4dd5898b46f44ac894a71230823bb
SamSumanth6 <sumanthvenkatesh6@gmail.com>
2023-11-30 15:59:18 +0530

    message

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './section.css';
 import Card from '../card';
-import Carousel from '../Carousel/index';
+import Carousel from '../Carousel';
 
 const Section = ({ title, data, navId }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
